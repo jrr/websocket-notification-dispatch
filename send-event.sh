d93f610cc5c1f6f6eb6dcdb39579594fc996f9f9
John Ruble <john.ruble@atomicobject.com>
2021-11-03 22:04:12 -0400

    send event with parameterized payload

--- send-event.sh
+++ send-event.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-AWS_REGION=us-west-1 aws events put-events --entries "file://event-bridge-event.json"
+if [ "$#" -ne 2 ]; then
+	echo "Usage:"
+	echo "./send-event.sh bob hello"
+	exit 1
+fi
+
+TO=$1
+MESSAGE=$2
+
+JSON="{ \\\"to\\\": \\\"$TO\\\", \\\"message\\\": \\\"$MESSAGE\\\" }"
+AWS_REGION=us-west-1 aws events put-events --entries "[{\"EventBusName\":\"websocket-test-bus\", \"Source\": \"local-dev\", \"DetailType\": \"example-event\", \"Detail\": \"$JSON\"}]"
